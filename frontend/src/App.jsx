3725336c5b7b6338094329d4a90975cc519b99cf
vaishali verma <vaishaliverma288@gmail.com>
2026-09-11 00:26:44 +0530

    feat: set up React frontend with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
